59aafc0953490ffb3112decf606806e6377ebc55
정승현 <tmdgusya@naver.com>
2023-05-24 15:09:01 +0900

    Made a base design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,56 @@
 
 export default function Home() {
-  return <>
-    <div className="h-full bg-gradient-to-b from-teal-300 font-lora font-serif">
+  return <div className="flex flex-col bg-gradient-to-b from-teal-300 font-lora font-serif items-center">
+    <div className="flex flex-col h-full">
       <div className="flex flex-nowrap pl-4 pt-1 items-center">
         <h1 className="w-30 h-20 text-7xl">Roach</h1>
-        <h1 className="w-30 ml-56 text-xl pt-8">Intro</h1>
+        <h1 className="w-30 ml-36 text-xl pt-8">Intro</h1>
         <h1 className="w-30 ml-20 text-xl pt-8">Posts</h1>
       </div>
-      <div>
-        <div>
-          <img className="rounded-full" src="https://avatars.githubusercontent.com/u/57784077?v=4" width="260" height="260"/>
-          <div>
+      <div className="flex flex-nowrap bg-red-500 h-0.5 w-full"></div>
+      <div className="flex flex-nowrap bg-red-500 mt-1 h-0.5 w-full"></div>
+      <div className="flex mt-2.5 max-w-5xl">
+        <div className="flex flex-nowrap flex-col">
+          <img className="rounded-full border-2 border-black max-w-xs max-h-xs" src="https://avatars.githubusercontent.com/u/57784077?v=4"/>
+          <div className="flex flex-wrap flex-col items-center max-w-xs">
             {/*Description In here*/}
             <p>
               Software Engineer
             </p>
+            <p>
+              I have been working as a software engineer for 2 years.
+            </p>
+            {/*Create Contact Button, has Round Space, and Clickable, Linkable*/}
+            <button className="bg-black text-white rounded-full mt-2.5 w-full h-10">Github</button>
+            <button className="bg-rose-600 text-white rounded-full mt-2.5 w-full h-10">Email</button>
+            <button className="bg-sky-900 text-white rounded-full mt-2.5 w-full h-10">Linked-In</button>
+            <button className="bg-sky-600 text-white rounded-full mt-2.5 w-full h-10">FaceBook</button>
+            <button className="bg-cyan-500 text-white rounded-full mt-2.5 w-full h-10">Twitter</button>
           </div>
         </div>
+        <div className="ml-10">
+            <p>
+                Sure, here is a 500-word technical article written in English that sounds like it was written by Martin Fowler:
+                The Future of Software Development
+                The field of software development is constantly evolving, and it can be difficult to keep up with the latest trends. However, there are a few key trends that are likely to shape the future of software development.
+                One of the most important trends is the rise of artificial intelligence (AI). AI is already being used in a variety of ways in software development, from automating tasks to generating code. As AI continues to develop, it is likely to play an even more significant role in software development.
+                Another important trend is the move to cloud computing. Cloud computing is becoming increasingly popular, as it offers a number of advantages over traditional on-premises infrastructure. Cloud computing can help businesses to save money, improve scalability, and get access to the latest technologies.
+                The rise of mobile devices is also having a major impact on software development. Mobile devices are now the primary way that people interact with the internet, and businesses need to ensure that their websites and applications are optimized for mobile devices.
+                Finally, the growing importance of security is another key trend in software development. As software becomes more complex, it is becoming more vulnerable to attacks. Businesses need to make sure that they are taking steps to protect their software from security threats.
+                These are just a few of the key trends that are likely to shape the future of software development. The field of software development is constantly changing, and it is important to stay up-to-date on the latest trends in order to be successful.
+                In addition to the trends mentioned above, there are a number of other factors that are likely to shape the future of software development. These include the increasing popularity of open source software, the rise of agile development, and the growing importance of user experience.
+                The field of software development is constantly evolving, and it can be difficult to predict the future. However, the trends mentioned above provide a glimpse into what the future may hold.
+                The field of software development is constantly evolving, and it can be difficult to keep up with the latest trends. However, there are a few key trends that are likely to shape the future of software development.
+                One of the most important trends is the rise of artificial intelligence (AI). AI is already being used in a variety of ways in software development, from automating tasks to generating code. As AI continues to develop, it is likely to play an even more significant role in software development.
+                Another important trend is the move to cloud computing. Cloud computing is becoming increasingly popular, as it offers a number of advantages over traditional on-premises infrastructure. Cloud computing can help businesses to save money, improve scalability, and get access to the latest technologies.
+                The rise of mobile devices is also having a major impact on software development. Mobile devices are now the primary way that people interact with the internet, and businesses need to ensure that their websites and applications are optimized for mobile devices.
+                Finally, the growing importance of security is another key trend in software development. As software becomes more complex, it is becoming more vulnerable to attacks. Businesses need to make sure that they are taking steps to protect their software from security threats.
+                These are just a few of the key trends that are likely to shape the future of software development. The field of software development is constantly changing, and it is important to stay up-to-date on the latest trends in order to be successful.
+                In addition to the trends mentioned above, there are a number of other factors that are likely to shape the future of software development. These include the increasing popularity of open source software, the rise of agile development, and the growing importance of user experience.
+                The field of software development is constantly evolving, and it can be difficult to predict the future. However, the trends mentioned above provide a glimpse into what the future may hold.
+              </p>
+          </div>
       </div>
     </div>
-  </>
+  </div>
 }

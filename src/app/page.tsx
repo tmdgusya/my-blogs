66fe951a9b1fc4c7952c3a7fe2ae3f65a4295bf2
정승현 <tmdgusya@naver.com>
2023-05-24 18:54:43 +0900

    Base Design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,36 +1,41 @@
+import Tap from "./components/tap";
 
 export default function Home() {
-  return <div className="flex flex-col bg-gradient-to-b from-teal-300 font-lora font-serif items-center">
+  {/*bg-gradient-to-b from-teal-300 */}
+  return <div className="flex flex-col font-lora antialiased font-serif items-center">
     <div className="flex flex-col h-full">
-      <div className="flex flex-nowrap pl-4 pt-1 items-center">
+      <div className="flex flex-nowrap pt-1 items-center">
         <h1 className="w-30 h-20 text-7xl">Roach</h1>
-        <h1 className="w-30 ml-36 text-xl pt-8">Intro</h1>
-        <h1 className="w-30 ml-20 text-xl pt-8">Posts</h1>
+        <a className="w-30 ml-40 text-xl pt-8">Intro</a>
+        <a className="w-30 ml-20 text-xl pt-8">Posts</a>
+        <a className="w-30 ml-20 text-xl pt-8">Series</a>
       </div>
       <div className="flex flex-nowrap bg-red-500 h-0.5 w-full"></div>
       <div className="flex flex-nowrap bg-red-500 mt-1 h-0.5 w-full"></div>
-      <div className="flex mt-2.5 max-w-5xl">
+      <div className="flex mt-5 max-w-5xl">
         <div className="flex flex-nowrap flex-col">
           <img className="rounded-full border-2 border-black max-w-xs max-h-xs" src="https://avatars.githubusercontent.com/u/57784077?v=4"/>
           <div className="flex flex-wrap flex-col items-center max-w-xs">
             {/*Description In here*/}
-            <p>
+            <p className="font-bold mt-2">
               Software Engineer
             </p>
-            <p>
-              I have been working as a software engineer for 2 years.
+            <p className="mt-3">
+              I have been working as a software engineer for 2 years. 
+              I like conversations about software engineering and programming. 
             </p>
-            {/*Create Contact Button, has Round Space, and Clickable, Linkable*/}
-            <button className="bg-black text-white rounded-full mt-2.5 w-full h-10">Github</button>
-            <button className="bg-rose-600 text-white rounded-full mt-2.5 w-full h-10">Email</button>
-            <button className="bg-sky-900 text-white rounded-full mt-2.5 w-full h-10">Linked-In</button>
-            <button className="bg-sky-600 text-white rounded-full mt-2.5 w-full h-10">FaceBook</button>
-            <button className="bg-cyan-500 text-white rounded-full mt-2.5 w-full h-10">Twitter</button>
+            <div className="mt-3 w-full">
+              <button className="bg-black text-white rounded-full mt-2.5 w-full h-10">Github</button>
+              <button className="bg-rose-600 text-white rounded-full mt-2.5 w-full h-10">Email</button>
+              <button className="bg-sky-900 text-white rounded-full mt-2.5 w-full h-10">Linked-In</button>
+              <button className="bg-sky-600 text-white rounded-full mt-2.5 w-full h-10">FaceBook</button>
+              <button className="bg-cyan-500 text-white rounded-full mt-2.5 w-full h-10">Twitter</button>
+            </div>
           </div>
         </div>
         <div className="ml-10">
             <p>
-                Sure, here is a 500-word technical article written in English that sounds like it was written by Martin Fowler:
+                Sure, here is a 500-word technical article written in English that sounds like it was written by Martin Fowler: {/*add hover feature like wikipedia*/}<Tap link="http://www.naver.com" description="[1]"/>
                 The Future of Software Development
                 The field of software development is constantly evolving, and it can be difficult to keep up with the latest trends. However, there are a few key trends that are likely to shape the future of software development.
                 One of the most important trends is the rise of artificial intelligence (AI). AI is already being used in a variety of ways in software development, from automating tasks to generating code. As AI continues to develop, it is likely to play an even more significant role in software development.

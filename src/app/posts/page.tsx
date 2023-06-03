cbeecbc18560792272d086a5d5256625d8148919
정승현 <tmdgusya@naver.com>
2023-06-03 18:13:10 +0900

    Set media size of posts to 1

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -3,7 +3,7 @@ import Post from "../components/posts/Post";
 
 export default function Posts() {
     return <RootLayout>
-        <div className="grid grid-cols-3 gap-5 max-w-full">
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-5 max-w-full">
             <Post title="Introduce Kotlin Ksp" description="Kotlin ksp is" img="https://avatars.githubusercontent.com/u/57784077?v=4" href="/kotlin/ksp/1"/>
             <Post title="Introduce Kotlin Ksp" description="Kotlin ksp is" img="https://avatars.githubusercontent.com/u/57784077?v=4" href="/kotlin/ksp/1"/>
             <Post title="Introduce Kotlin Ksp" description="Kotlin ksp is" img="https://avatars.githubusercontent.com/u/57784077?v=4" href="/kotlin/ksp/1"/>

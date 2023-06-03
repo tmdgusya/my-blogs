a662ce672babc5754aa462fdecdf7052838ce260
정승현 <tmdgusya@naver.com>
2023-06-03 18:50:27 +0900

    Fixing file name casing

--- src/app/components/wiki/bold.tsx
+++
@@ -1,9 +0,0 @@
-interface BoldProps {
-    text: string;
-}
-
-export default function Bold({ text }: BoldProps) {
-    return (
-        <span className="font-bold antialiased">{text}</span>
-    )
-}
--- src/app/kotlin/ksp/1/Page.tsx
+++
@@ -1,9 +0,0 @@
-import RootLayout from "@/app/layout";
-
-export default function KspIntro() {
-    return <RootLayout>
-        <div className="">
-        <span className="box-decoration-slice bg-gradient-to-r from-indigo-600 to-pink-500 text-white px-2 ...">Hello World</span>
-        </div>
-    </RootLayout>
-}
